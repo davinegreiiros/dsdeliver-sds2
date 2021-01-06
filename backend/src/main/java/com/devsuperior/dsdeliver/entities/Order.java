0987f3c8dc0bde08d30a373bb2f4f3b2bfc4d9fc
davinegreiiros <davi.negreiiros36@gmail.com>
2021-01-06 01:04:21 -0300

    GET /orders

--- backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
@@ -14,8 +14,6 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
-import com.devsuperior.dsdeliver.OrderStatus;
-
 @Entity
 @Table(name = "tb_order")
 public class Order implements Serializable{
--- backend/src/main/java/com/devsuperior/dsdeliver/entities/OrderStatus.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/entities/OrderStatus.java
@@ -1,4 +1,4 @@
-package com.devsuperior.dsdeliver;
+package com.devsuperior.dsdeliver.entities;
 
 public enum OrderStatus {
 
