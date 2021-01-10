c171c993c24bb7b5d4250b2ba5b192bd8fb08ee4
davinegreiiros <davi.negreiiros36@gmail.com>
2021-01-10 19:24:32 -0300

    Finish front-web

--- front-web/src/Orders/OrderLocation.tsx
+++ front-web/src/Orders/OrderLocation.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import AsyncSelect from 'react-select/async';
 import { fetchLocalMapBox } from "../api";
-import { OrderLocationdata } from "./Types";
+import { OrderLocationData } from "./Types";
 
 const initialPosition = {
   lat: -3.7573136,
@@ -19,7 +19,7 @@ type Place = {
 }
 
 type Props = {
-  onChangeLocation: (location: OrderLocationdata) => void;
+  onChangeLocation: (location: OrderLocationData) => void;
 }
 
 function OrderLocation({ onChangeLocation }: Props) {
